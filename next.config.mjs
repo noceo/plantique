65374b1c3c8638884caa430dfeaa5c7a72bb1f6a
noceo <paulschade20@gmail.com>
2024-05-13 17:00:16 -0400

    added svgr dependency

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,15 @@ const nextConfig = {
   images: {
     domains: ["placehold.co"],
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
