7ac4ff0745c0ab4ba7f275157bc207f7171efdd8
noceo <paulschade20@gmail.com>
2024-05-23 19:23:02 -0400

    added new button styles

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,39 +1,48 @@
 "use client";
 
+import classNames from "classnames";
 import Link from "next/link";
-import { AnchorHTMLAttributes, ButtonHTMLAttributes } from "react";
+import { AnchorHTMLAttributes, ButtonHTMLAttributes, ReactNode } from "react";
 
 type ButtonAnchorIntersection = ButtonHTMLAttributes<HTMLButtonElement> &
   AnchorHTMLAttributes<HTMLAnchorElement>;
 
 interface ButtonProps extends ButtonAnchorIntersection {
-  variant: "primary" | "secondary" | "icon-only";
+  variant: "primary" | "secondary" | "icon-only" | "icon-square" | "icon-link";
+  color?: "success" | "danger";
+  icon?: ReactNode;
   href?: string;
 }
 
 export default function Button({
   variant,
+  color,
+  icon,
   href,
   children,
   ...props
 }: ButtonProps) {
+  const colorClass = color ? `btn--${color}` : undefined;
+  const btnClass = classNames("btn", `btn--${variant}`, colorClass);
+
   if (href) {
     if (href.startsWith("http") || href.startsWith("www")) {
       return (
-        <a {...props} href={href} className={"btn " + ("btn--" + variant)}>
+        <a {...props} href={href} className={btnClass} tabIndex={0}>
           {children}
         </a>
       );
     }
     return (
-      <Link {...props} href={href} className={"btn " + ("btn--" + variant)}>
+      <Link {...props} href={href} className={btnClass} tabIndex={0}>
         {children}
       </Link>
     );
   }
 
   return (
-    <button {...props} className={"btn " + ("btn--" + variant)} tabIndex={0}>
+    <button {...props} className={btnClass} tabIndex={0}>
+      {icon && <div className="btn__icon">{icon}</div>}
       {children}
     </button>
   );
